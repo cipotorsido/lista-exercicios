0ebbf86ba73eab6bd4b034ce4f55300fc45c4cc8
João Paulo Simioni <jpsimi69@gmail.com>
2023-03-08 21:55:31 -0400

    começo do exercicio

---
+++ exercicio1.py
@@ -0,0 +1,3 @@
+nome = str(input(" Qual o seu nome ?"))
+turno_de_trabalho = str(input(" Em qual periodo você trabalha ? (m = Matutino), (v = Vespertino), (n = Noturno) "))
+categoria = str(input(" Você é gerente(g) ou operario(o) ? "))
