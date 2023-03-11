9687f6699b417617ac35e77d6dec865984f5e8a8
João Paulo Simioni <jpsimi69@gmail.com>
2023-03-11 00:40:53 -0400

    Atualização e finalização do exercicio 1 de programação.

--- exercicio1.py
+++ exercicio1.py
@@ -1,3 +1,37 @@
-nome = str(input(" Qual o seu nome ?"))
-turno_de_trabalho = str(input(" Em qual periodo você trabalha ? (m = Matutino), (v = Vespertino), (n = Noturno) "))
-categoria = str(input(" Você é gerente(g) ou operario(o) ? "))
+pessoas = []
+while True:
+    pessoa = {}
+    pessoa['nome'] = str(input("Qual o seu nome? SEM ACENTOS"))
+    if not pessoa['nome'].isalpha():
+        print("Seu nome não condiz com o cartório")
+        continue
+
+    pessoa['turno_de_trabalho'] = str(input("Em qual período você trabalha? (m = Matutino), (v = Vespertino), (n = Noturno) "))
+    if pessoa['turno_de_trabalho'] not in ["m", "v", "n"]:
+        print("Seu período não condiz com os disponíveis")
+        continue
+
+    pessoa['categoria'] = str(input("Você é gerente(g) ou operário(o)? "))
+    if pessoa['categoria'] not in ["g", "o"]:
+        print("Opção inválida")
+        continue
+
+    pessoa['salario'] = 0
+
+    if pessoa['categoria'] == 'g' and pessoa['turno_de_trabalho'] == 'n':
+        pessoa['salario'] = 0.1 * 1320
+    elif pessoa['categoria'] == 'g' and (pessoa['turno_de_trabalho'] == 'm' or pessoa['turno_de_trabalho'] == 'v'):
+        pessoa['salario'] = 0.15 * 1320
+    elif pessoa['categoria'] == 'o' and (pessoa['turno_de_trabalho'] == 'n'):
+        pessoa['salario'] = 0.09 * 1320
+    elif pessoa['categoria'] == 'o' and (pessoa['turno_de_trabalho'] == 'm' or pessoa['turno_de_trabalho'] == 'v'):
+        pessoa['salario'] = 0.14 * 1320
+
+    pessoas.append(pessoa)
+    resposta = input("Deseja adicionar informações de outra pessoa? (s/n) ")
+    if resposta.lower() == 'n':
+        break
+
+
+for pessoa in pessoas:
+    print(f"Nome: {pessoa['nome']}, Salário: R${pessoa['salario']:.2f}")
